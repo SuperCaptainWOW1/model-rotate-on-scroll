7ef8b3f9972160da5e387a46ebc0bdfee4b9885f
Vladimir Makhortov <makhortov.dev@gmail.com>
2023-09-15 17:10:41 +0300

    change maxRotation

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-const maxRotation = Math.PI;
+const maxRotation = Math.PI * 0.42;
 
 function startRender() {
   const scene = new THREE.Scene();
